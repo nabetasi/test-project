2966be2d77d29e6e36987ed59c8bfac0cec73585
nabeta <masaomi.nabeta@gmail.com>
2019-10-13 10:42:31 +0900

    calc_cn rule_3 fixed

--- poly/poly.py
+++ poly/poly.py
@@ -20,10 +20,12 @@ class polygon:
         #pts1 = np.float32([[0,0],[140,0],[0,70]])
     
     
-    def calc_cn(self, check_point=[0.0,0.0]):
+    def calc_cn(self, check_point=[90.0,250.0]):
         print("check_point:" + str(check_point))
         check_x = check_point[0]
         check_y = check_point[1]
+
+        cn = 0
 
         for point in range(len(self.poly)-1):
             print("loop:" + str(point))
@@ -37,16 +39,19 @@ class polygon:
             print("next y=" + str(next_pos_y))
             rule_1 = (now_pos_y <= check_y) and (next_pos_y > check_y)
             print("ruler_1 : " + str(rule_1))
-            rule_2 = (now_pos_y < check_y) and (next_pos_y <= check_y)
+            rule_2 = (now_pos_y > check_y) and (next_pos_y <= check_y)
             print("ruler_2 : " + str(rule_2))
-            if rule_1 and rule_2:
+            if rule_1 or rule_2:
                 vt = (check_y - now_pos_y) / (next_pos_y - now_pos_y)
                 target_x = vt * (next_pos_x - now_pos_x) + now_pos_x
                 print("target_x=" + str(target_x))
                 rule_3 = check_x < target_x
                 print("rule_3 : " + str(rule_3))
+                if rule_3: cn += 1
             else:
                 print("rule_3 is not evaluated")
+        
+        print("cn = " + str(cn))
 
 
 def main(args):
