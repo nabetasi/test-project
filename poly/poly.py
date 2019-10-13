5b92234e27cea92aef43fb7a9beb90fbdeb6308b
nabeta <masaomi.nabeta@gmail.com>
2019-10-13 11:05:45 +0900

    in polygon judge function

--- poly/poly.py
+++ poly/poly.py
@@ -28,37 +28,55 @@ class polygon:
         cn = 0
 
         for point in range(len(self.poly)-1):
-            print("loop:" + str(point))
+            #print("loop:" + str(point))
             now_pos_x = self.poly[point][0]
             now_pos_y = self.poly[point][1]
-            print("now x=" + str(now_pos_x))
-            print("now y=" + str(now_pos_y))
+            #print("now x=" + str(now_pos_x))
+            #print("now y=" + str(now_pos_y))
             next_pos_x = self.poly[point+1][0]
             next_pos_y = self.poly[point+1][1]
-            print("next x=" + str(next_pos_x))
-            print("next y=" + str(next_pos_y))
+            #print("next x=" + str(next_pos_x))
+            #print("next y=" + str(next_pos_y))
             rule_1 = (now_pos_y <= check_y) and (next_pos_y > check_y)
-            print("ruler_1 : " + str(rule_1))
+            #print("ruler_1 : " + str(rule_1))
             rule_2 = (now_pos_y > check_y) and (next_pos_y <= check_y)
-            print("ruler_2 : " + str(rule_2))
+            #print("ruler_2 : " + str(rule_2))
             if rule_1 or rule_2:
                 vt = (check_y - now_pos_y) / (next_pos_y - now_pos_y)
                 target_x = vt * (next_pos_x - now_pos_x) + now_pos_x
-                print("target_x=" + str(target_x))
+                #print("target_x=" + str(target_x))
                 rule_3 = check_x < target_x
-                print("rule_3 : " + str(rule_3))
+                #print("rule_3 : " + str(rule_3))
                 if rule_3: cn += 1
-            else:
-                print("rule_3 is not evaluated")
-        
-        print("cn = " + str(cn))
+            #else:
+                #print("rule_3 is not evaluated")
+        #print("cn = " + str(cn))
+        return cn
+
+    def is_in_polygon(self, check_point=[0.0,0.0]):
+        cn = self.calc_cn(check_point)
+        if cn % 2 == 0:
+            print("Out polygon")
+            return False
+        else:
+            print("In polygon")
+            return True
 
 
 def main(args):
     print("main start")
     ply = polygon()
     print("calc cn")
-    ply.calc_cn()
+    check = ply.is_in_polygon([0.0,0.0])
+    print("point1 is : " + str(check))
+    check = ply.is_in_polygon([150.0,250.0])
+    print("point2 is : " + str(check))
+    check = ply.is_in_polygon([300.0,250.0])
+    print("point3 is : " + str(check))
+    check = ply.is_in_polygon([101.0,200.0])
+    print("point4 is : " + str(check))
+
+
 
 
 if __name__ == '__main__':
